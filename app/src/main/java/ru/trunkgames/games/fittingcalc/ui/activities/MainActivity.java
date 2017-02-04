967c5cafa01cf5966db8617f0502b97a83627d95
Edik <popoveduard@yandex.ru>
2017-02-04 17:47:11 +0300

    Initial commit

---
+++ app/src/main/java/ru/trunkgames/games/fittingcalc/ui/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.trunkgames.games.fittingcalc.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+*/
+import ru.trunkgames.games.fittingcalc.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
